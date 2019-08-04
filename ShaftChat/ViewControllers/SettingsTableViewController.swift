076c9c3dcfa5bfb3af7cb7418b6e801dbf06c192
Rodrigo Sant Ana <er.7santana@gmail.com>
2019-08-04 04:02:07 -0300

    logout function

---
+++ ShaftChat/ViewControllers/SettingsTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SettingsTableViewController.swift
+//  ShaftChat
+//
+//  Created by Rodrigo Sant Ana on 04/08/19.
+//  Copyright © 2019 Shaft Corporation. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    //MARK: - Actions
+
+    @IBAction func logoutButtonPressed(_ sender: Any) {
+        
+        FUser.logOutCurrentUser { (success) in
+            if success {
+                self.showLoginView()
+            }
+        }
+        
+    }
+    
+    //MARK: - Helper functions
+    
+    func showLoginView(){
+        
+        let mainController = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "welcome")
+        
+        self.present(mainController, animated: true, completion: nil)
+    }
+    
+    
+}

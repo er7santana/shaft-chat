f0389d72cee94a21dc4c78edf77b34a42906f23c
Rodrigo Sant Ana <er.7santana@gmail.com>
2019-08-17 14:03:17 -0300

    display first recent chat

--- ShaftChat/ViewControllers/ChatsViewController.swift
+++ ShaftChat/ViewControllers/ChatsViewController.swift
@@ -7,13 +7,24 @@
 //
 
 import UIKit
+import Firebase
 
-class ChatsViewController: UIViewController {
+class ChatsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var recentChats: [NSDictionary] = []
+    var filteredChats: [NSDictionary] = []
+    var recentListener: ListenerRegistration!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        loadRecentChats()
     }
     
     //MARK: - Actions
@@ -25,5 +36,57 @@ class ChatsViewController: UIViewController {
         self.navigationController?.pushViewController(usersViewController, animated: true)
     }
     
+    //MARK: - TableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return recentChats.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "recentChatCell", for: indexPath) as! RecentChatsTableViewCell
+        
+        let recentChat = recentChats[indexPath.row]
+        
+        cell.generateCell(recentChat: recentChat, indexPath: indexPath)
+        
+        return cell;
+    }
+    
+    //MARK: - Get Recent Chats
+    
+    func loadRecentChats() {
+        
+        recentListener = reference(.Recent).whereField(kUSERID, isEqualTo: FUser.currentId()).addSnapshotListener({ (snapshot, error) in
+            
+            if error != nil {
+                print(error!.localizedDescription)
+                return
+            }
+            
+            guard let snapshot = snapshot else { return }
+            
+            self.recentChats = []
+            
+            if !snapshot.isEmpty {
+                
+                let sorted = ((dictionaryFromSnapshots(snapshots: snapshot.documents)) as NSArray).sortedArray(using: [NSSortDescriptor(key: kDATE, ascending: false)]) as! [NSDictionary]
+                
+                for recent in sorted {
+                    
+                    if recent[kLASTMESSAGE] as! String != "" && recent[kCHATROOMID] as! String != "" && recent[kRECENTID] as! String != "" {
+                        
+                        self.recentChats.append(recent)
+                    }
+                }
+                
+//                self.tableView.reloadData()
+            }
+            
+            self.tableView.reloadData()
+            
+        })
+        
+    }
     
 }

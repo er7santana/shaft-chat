8bebfe826e1dac20af6fe0fbd6f95803542da59a
Rodrigo Sant Ana <er.7santana@gmail.com>
2019-08-18 20:29:50 -0300

    incoming message class for text messages

---
+++ ShaftChat/Models/IncomingMessages.swift
@@ -0,0 +1,77 @@
+//
+//  IncomingMessages.swift
+//  ShaftChat
+//
+//  Created by Rodrigo Sant Ana on 18/08/19.
+//  Copyright © 2019 Shaft Corporation. All rights reserved.
+//
+
+import Foundation
+import JSQMessagesViewController
+
+class IncomingMessages {
+    
+    var collectionView: JSQMessagesCollectionView
+    
+    init(_collectionView: JSQMessagesCollectionView) {
+        collectionView = _collectionView
+    }
+    
+    //MARK: - create message
+    
+    func createMessage(messageDictionary: NSDictionary, chatRoomId: String) -> JSQMessage? {
+        
+        var message: JSQMessage?
+        
+        let type = messageDictionary[kTYPE] as! String
+        
+        switch type {
+        case kTEXT:
+            message = createTextMessage(messageDictionary: messageDictionary, chatRoomId: chatRoomId)
+        case kPICTURE:
+            //create picture message
+            print("picture message")
+        case kVIDEO:
+            //create video message
+            print("video message")
+        case kAUDIO:
+            //create audio message
+            print("audio message")
+        case kLOCATION:
+            //create location message
+            print("location message")
+        default:
+            print("unknown message type")
+        }
+        
+        if message != nil {
+            return message
+        }
+        
+        return nil
+        
+    }
+    
+    //MARK: - create message types
+    
+    func createTextMessage(messageDictionary: NSDictionary, chatRoomId: String) -> JSQMessage {
+        let name = messageDictionary[kSENDERNAME] as? String
+        let userId = messageDictionary[kUSERID] as? String
+        
+        var date: Date!
+        
+        if let created = messageDictionary[kDATE] {
+            if (created as! String).count != 14 {
+                date = Date()
+            }else {
+                date = dateFormatter().date(from: created as! String)
+            }
+            
+        } else {
+            date = Date()
+        }
+        
+        return JSQMessage(senderId: userId, senderDisplayName: name, date: date, text: (messageDictionary[kMESSAGE] as! String))
+    }
+    
+}

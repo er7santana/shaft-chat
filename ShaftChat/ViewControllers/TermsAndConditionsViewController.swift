def620dad36bbbd46ea06394b8c52ab690df916e
Eliezer Rodrigo <er.7santana@gmail.com>
2019-09-03 23:45:28 -0300

    terms and condition page

---
+++ ShaftChat/ViewControllers/TermsAndConditionsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TermsAndConditionsViewController.swift
+//  ShaftChat
+//
+//  Created by Eliezer Rodrigo on 03/09/19.
+//  Copyright © 2019 Shaft Corporation. All rights reserved.
+//
+
+import UIKit
+
+class TermsAndConditionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.largeTitleDisplayMode = .never
+    }
+    
+}

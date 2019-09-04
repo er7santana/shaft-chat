b384282be83e6dde07026f0b7d35d6a233883489
Eliezer Rodrigo <er.7santana@gmail.com>
2019-09-03 22:18:22 -0300

    unblocking users from blocked view

---
+++ ShaftChat/ViewControllers/BlockedUsersViewController.swift
@@ -0,0 +1,104 @@
+//
+//  BlockedUsersViewController.swift
+//  ShaftChat
+//
+//  Created by Eliezer Rodrigo on 03/09/19.
+//  Copyright © 2019 Shaft Corporation. All rights reserved.
+//
+
+import UIKit
+import ProgressHUD
+
+class BlockedUsersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UserTableViewCellDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var blockedUsersArray: [FUser] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+        tableView.tableFooterView = UIView()
+        loadUsers()
+    }
+    
+    //MARK: - Table view data source
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return blockedUsersArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userTableViewCell", for: indexPath) as! UserTableViewCell
+        
+        cell.delegate = self
+        cell.generateCellWith(fUser: blockedUsersArray[indexPath.row], indexPath: indexPath)
+        
+        return cell
+    }
+    
+    //MARK: - Table view delegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "Unblock"
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        var tempBlockUsers = FUser.currentUser()!.blockedUsers
+        let userIdToUnblock = blockedUsersArray[indexPath.row].objectId
+        tempBlockUsers.remove(at: tempBlockUsers.firstIndex(of: userIdToUnblock)!)
+        blockedUsersArray.remove(at: indexPath.row)
+        
+        updateCurrentUserInFirestore(withValues: [kBLOCKEDUSERID : tempBlockUsers]) { (error) in
+            
+            if error != nil {
+                print(error!.localizedDescription)
+                ProgressHUD.showError(error!.localizedDescription)
+            }
+            
+            self.tableView.reloadData()
+        }
+    }
+    
+    //MARK: - Load blocked users
+    
+    func loadUsers() {
+        
+        if FUser.currentUser()!.blockedUsers.count > 0 {
+            
+            ProgressHUD.show()
+            
+            getUsersFromFirestore(withIds: FUser.currentUser()!.blockedUsers) { (allBlockedUsers) in
+                
+                ProgressHUD.dismiss()
+                self.blockedUsersArray = allBlockedUsers
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    
+    //MARK: - UserTableViewCellDelegate
+    
+    func didTapAvatarImage(indexPath: IndexPath) {
+        
+        let profileViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "profileView") as! ProfileViewTableViewController
+        profileViewController.user = blockedUsersArray[indexPath.row]
+        
+        self.navigationController?.pushViewController(profileViewController, animated: true)
+    }
+    
+    
+}

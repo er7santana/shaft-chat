e385b443a76fbdab94974b4ea5135794831ac5c2
Eliezer Rodrigo <er.7santana@gmail.com>
2019-09-10 19:37:37 -0300

    call view controller

--- ShaftChat/ViewControllers/CallViewController.swift
+++ ShaftChat/ViewControllers/CallViewController.swift
@@ -26,9 +26,37 @@ class CallViewController: UIViewController, SINCallDelegate {
     var callAnswered = false
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        userNameLabel.text = "Unknown"
+        let id = _call.remoteUserId
+        getUsersFromFirestore(withIds: [id!]) { (allUsers) in
+            if allUsers.count > 0 {
+                let user = allUsers.first!
+                self.userNameLabel.text = user.fullname
+                imageFromData(pictureData: user.avatar, withBlock: { (avatarImage) in
+                    if avatarImage != nil {
+                        self.avatarImageView.image = avatarImage!.circleMasked
+                    }
+                })
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        _call.delegate = self
+        if _call.direction == .incoming {
+            showButtons()
+            
+        } else {
+            callAnswered = true
+            setCallStatus(text: "Calling...")
+            showButtons()
+        }
     }
     
     //MARK: - IBActions
@@ -48,4 +76,25 @@ class CallViewController: UIViewController, SINCallDelegate {
     @IBAction func declineButtonPressed(_ sender: Any) {
     }
     
+    //MARK: - Update UI
+    
+    func setCallStatus(text: String) {
+        statusLabel.text = text
+    }
+    
+    func showButtons() {
+        if callAnswered {
+            declineButton.isHidden = true
+            endCallButton.isHidden = false
+            answerButton.isHidden = true
+            muteButton.isHidden = false
+            speakerButton.isHidden = false
+        } else {
+            declineButton.isHidden = false
+            endCallButton.isHidden = true
+            answerButton.isHidden = false
+            muteButton.isHidden = true
+            speakerButton.isHidden = true
+        }
+    }
 }
